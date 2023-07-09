883f146e8137c124edbdcbb6fd1d0c07a519bbd7
Tobi Lehman <mail@tobilehman.com>
2023-07-09 12:26:04 -0700

    Rearrange file

--- src/pls/core.clj
+++ src/pls/core.clj
@@ -2,22 +2,9 @@
   (:require [clojure.java.io :as io])
   (:require [clojure.string :as s])
   (:require [clj-http.client :as client])
-  (:import [java.io BufferedOutputStream FileOutputStream]))
+  (:import [java.io BufferedOutputStream FileOutputStream])
+  (:import [javax.sound.sampled AudioFormat AudioSystem LineUnavailableException SourceDataLine DataLine DataLine$Info]))
 
-(defn entry-number [line]
-  "PLS files have an entry number like File1 or Title1, non-entries return -1"
-  (let [n (second (re-find #"(\d+)?=" line))]
-    (if (nil? n)
-      -1
-      (Integer/parseInt n))))
-
-(defn file-to-lines [filename]
-  "Read a PLS file and filter only the TitleN= and FileN= lines"
-  (filter
-   (fn [s] (re-find #"^(Title|File)" s))
-   (with-open [rdr (io/reader filename)]
-     (doall
-      (line-seq rdr)))))
 
 (defn parse-file [filename]
   "Parse a PLS file into a list of {:title :file} maps"
@@ -41,22 +28,34 @@
   (->> filename
        (parse-file)
        (map :file)))
-#'pls.core/get-urls-from-pls-file
+
+
+(defn file-to-lines [filename]
+  "Read a PLS file and filter only the TitleN= and FileN= lines"
+  (filter
+   (fn [s] (re-find #"^(Title|File)" s))
+   (with-open [rdr (io/reader filename)]
+     (doall
+      (line-seq rdr)))))
+
+(defn entry-number [line]
+  "PLS files have an entry number like File1 or Title1, non-entries return -1"
+  (let [n (second (re-find #"(\d+)?=" line))]
+    (if (nil? n)
+      -1
+      (Integer/parseInt n))))
+
 
 (first (get-urls-from-pls-file "/Users/tobi/Music/di.pls"))
 
 (get-genres-from-pls-file "/Users/tobi/Music/di.pls")
-("drumandbass" "liquiddubstep" "synthwave" "breaks" "progressivepsy" "detroithousentechno" "bassline" "chillout" "vocaltrance" "chillhop" "spacemusic" "chillntropicalhouse" "vocalchillout" "melodicprogressive" "oldschoolacid" "clubdubstep" "atmosphericbreaks" "progressive" "glitchhop" "darkpsytrance" "deephouse" "deepnudisco" "goapsy" "downtempolounge")
 
-;; TODO get the binary data from the stream and display it
+;; DONE get the binary data from the stream and display it
 ;; TODO write the binary data to an audio stream and then play it
 
 (let [url (first (get-urls-from-pls-file "/Users/tobi/Music/di.pls"))
       headers (:headers (client/head url))]
   (get headers "icy-genre"))
-"Drum and Bass"
-"Chill & Tropical House"
-
 
 ;;current-time (System/currentTimeMillis)
 ;;output-filename (format "/Users/tobi/Desktop/%s-%d.wav" genre current-time)
@@ -70,5 +69,3 @@
   (doall
     (.read input-stream buffer)
     (into [] buffer)))
-
-[-1 -15 80 -128 46 31 -52 33 26 -113 -3 -6 -73 -1 -31 69]
